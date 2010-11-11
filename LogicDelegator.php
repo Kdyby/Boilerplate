ba6dbc11c16d4db9665f81574a3af04d4cae3a21
Filip Procházka (HosipLan) <HosipLan@gmail.com>
2010-11-11 16:31:52 +0100

    added LogicDelegator for delegating logic into template

---
+++ LogicDelegator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kdyby;
+
+use Nette;
+use Kdyby;
+
+
+
+/**
+ * Description of LogicDelegator
+ *
+ * @author Filip Procházka <hosiplan@kdyby.org>
+ */
+class LogicDelegator extends Nette\Object
+{
+	
+}
