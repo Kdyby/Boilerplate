cd4b193d0c3dbb3239aa210c41312e846b1a5171
Filip Procházka (HosipLan) <HosipLan@gmail.com>
2010-12-10 00:46:02 +0100

    Added properly-working classes to KdybyLoader

--- Loaders/KdybyLoader.php
+++ Loaders/KdybyLoader.php
@@ -17,20 +17,53 @@ class KdybyLoader extends Nette\Loaders\AutoLoader
 
 	/** @var array */
 	public $list = array(
+		//core
 		'kdyby\configurator' => '/Environment/Configurator.php',
-		'kdyby\filesystem' => '/Tools/FileSystem.php',
+		'kdyby\component\helpers' => "/Components/Helpers.php",
+		'kdyby\control\basecontrol' => '/Control/BaseControl.php',
+		'kdyby\control\lookoutcontrol' => '/Control/LookoutControl.php',
 		'kdyby\filestorage' => '/Caching/FileStorage.php',
+		'kdyby\filesystem' => '/Tools/FileSystem.php',
+		'kdyby\form\baseform' => '/Forms/BaseForm.php',
+		'kdyby\form\control\checkboxlist' => '/Forms/Controls/CheckboxList.php',
+		'kdyby\gateway\gateway' => '/Gateway/Gateway.php',
+		'kdyby\gateway\iadapter' => '/Gateway/Interfaces/IAdapter.php',
+		'kdyby\gateway\igateway' => '/Gateway/Interfaces/IGateway.php',
+		'kdyby\gateway\igatewayauthenticator' => '/Gateway/Interfaces/IGatewayAuthenticator.php',
+		'kdyby\gateway\igateways' => '/Gateway/Interfaces/IGateways.php',
+		'kdyby\gateway\irequest' => '/Gateway/Interfaces/IRequest.php',
+		'kdyby\gateway\iresponse' => '/Gateway/Interfaces/IResponse.php',
+		'kdyby\gateway\isecuredgateway' => '/Gateway/Interfaces/ISecuredGateway.php',
+		'kdyby\gateway\isecuredrequest' => '/Gateway/Interfaces/ISecuredRequest.php',
+		'kdyby\gateway\iservice' => '/Gateway/Interfaces/IService.php',
+		'kdyby\gateway\notauthenticatedexception' => '/Gateway/NotAuthenticatedException.php',
+		'kdyby\gateway\protocol\soap' => '/Gateway/Protocols/Soap.php',
+		'kdyby\gateway\protocol\iprotocol' => '/Gateway/Interfaces/IProtocol.php',
+		'kdyby\gateway\request' => '/Gateway/Request.php',
+		'kdyby\gateway\response' => '/Gateway/Response.php',
+		'kdyby\gateway\securedgateway' => '/Gateway/SecuredGateway.php',
+		'kdyby\gateway\service' => '/Gateway/Service.php',
 		'kdyby\logicdelegator' => '/Tools/LogicDelegator.php',
 		'kdyby\presenter\base' => '/Presenters/Base.php',
 		'kdyby\presenterinfo' => '/Tools/PresenterTree/PresenterInfo.php',
 		'kdyby\presentertree' => '/Tools/PresenterTree/PresenterTree.php',
 		'kdyby\security\applicationlock' => '/Security/ApplicationLock.php',
+		'kdyby\security\authenticator' => '/Security/Authenticator.php',
+		'kdyby\security\user' => '/Security/User.php',
+		'kdyby\tools\logicdelegator' => '/Kdyby/Tools/LogicDelegator.php',
+		'kdyby\tools\presentergenerator' => '/Kdyby/Tools/PresenterGenerator.php',
+		'kdyby\tools\modeltools' => '/Kdyby/Tools/ModelTools.php',
+
+		//components
+		'kdyby\component\headjs' => "/Components/Headjs/Headjs.php",
+		'kdyby\component\tree' => "/Components/Tree/Tree.php",
 
 		// doctrine
 		'kdyby\application\databasemanager' => '/Doctrine/DatabaseManager.php',
 		'kdyby\doctrine\cache' => '/Doctrine/Cache.php',
 		'kdyby\doctrine\factory' => '/Doctrine/Factory.php',
 		'kdyby\entities\baseentity' => '/Doctrine/BaseEntity.php',
+		'nella\doctrine\panel' => '/Doctrine/Panel.php'
 	);
 
 
