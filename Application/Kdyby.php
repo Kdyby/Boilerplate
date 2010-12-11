4e9d44e0ead9d0bf049375550c200fb43bfe41f9
Filip Procházka (HosipLan) <HosipLan@gmail.com>
2010-12-11 06:04:25 +0100

    Kdyby Application

---
+++ Application/Kdyby.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the Framework - Content Managing System (F-CMS) Kdyby.
+ *
+ * Copyright (c) 2008, 2010 Filip Procházka (http://hosiplan.kdyby.org)
+ *
+ * For more information please see http://www.kdyby.org
+ *
+ * @package F-CMS Kdyby-Common
+ */
+
+
+
+namespace Kdyby\Application;
+
+use Nette;
+use Nette\Environment;
+
+
+
+final class Kdyby extends Nette\Application\Application
+{
+	/** @var string */
+	public $errorPresenter = 'Error';
+
+
+}
