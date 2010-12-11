22ddbceb6cc19930db0e511822cb1a5793b2043c
Filip Procházka (HosipLan) <HosipLan@gmail.com>
2010-12-11 06:04:12 +0100

    AdminPanel skeleton

---
+++ AdminPanel/PresenterLoader.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the Framework - Content Managing System (F-CMS) Kdyby.
+ *
+ * Copyright (c) 2008, 2010 Filip Procházka (http://hosiplan.kdyby.org)
+ *
+ * For more information please see http://www.kdyby.org
+ *
+ * @package F-CMS Kdyby-Common
+ */
+
+
+namespace Kdyby\AdminPanel;
+
+use Nette;
+use Kdyby;
+
+
+
+/**
+ * @author Filip Procházka <hosiplan@kdyby.org>
+ */
+class PresenterLoader
+{
+	
+}
