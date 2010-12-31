b56dd41b95b7f8f718c0426865dfe1e4e776f73b
Filip Procházka (HosipLan) <HosipLan@gmail.com>
2010-12-31 14:48:01 +0100

    Kdyby\Configurator added createDatabaseManager(); registers Config parser for neon extension; added magic factory for services

--- Environment/Configurator.php
+++ Environment/Configurator.php
@@ -14,6 +14,7 @@
 namespace Kdyby;
 
 use Nette;
+use Nette\Environment;
 use Kdyby;
 
 
@@ -24,10 +25,132 @@ use Kdyby;
 class Configurator extends Nette\Configurator
 {
 
+	/** @var string */
+	private static $defaultServicesFile = "%appDir%/services.neon";
+
 	/** @var array */
 	private static $configHooks = array(
 		"Nette-Security-IIdentity" => "Kdyby\\Identity"
 	);
+
+
+
+	public function __construct()
+	{
+		Nette\Config\Config::registerExtension('neon', 'Kdyby\Config\ConfigAdapterNeon');
+	}
+
+
+
+	/**
+	 * @return Kdyby\Application\DatabaseManager
+	 */
+	public static function createDatabaseManager(array $options = NULL)
+	{
+		$dm = new Kdyby\Application\DatabaseManager;
+		$environmentName = Environment::getName();
+
+		$config = Environment::getConfig();
+		$context = Environment::getApplication()->getContext();
+		$em = $context->getService('Doctrine\ORM\EntityManager');
+		$dm->setEntityManager($em);
+		// Doctrine\DBAL\Types\Type::addType('set', 'Kdyby\Doctrine\Type\SetType');
+
+		$services = array();
+		$servicesFile = Environment::expand(isset($options['servicesFile']) ? $options['servicesFile'] : self::$defaultServicesFile);
+
+		// parse and load special services
+		if (file_exists($servicesFile)) {
+			$services = Nette\Config\Config::fromFile($servicesFile);
+		}
+
+		foreach ($services as $key => $value) {
+			$serviceName = strtr($key, '-', '\\'); // limited INI chars
+			$definition = is_string($value) ? array('class' => $value) : (array)$value;
+
+			$singleton = isset($value->singleton) ? (bool)$value->singleton : TRUE;
+			$options = array(
+				'definition' => $definition,
+				'context' => $context,
+				'config' => $config,
+				);
+
+			$dm->addService($serviceName, __CLASS__.'::serviceFactory', $singleton, $options);
+		}
+
+		return $dm;
+	}
+
+
+
+	/**
+	 * @author http://github.com/janmarek
+	 *
+	 * @param <type> $options
+	 * @return Service
+	 */
+	public static function serviceFactory($options)
+	{
+		$definition = $options['definition'];
+		$context = $options['context'];
+		$config = $options['config'];
+		$arguments = array();
+
+		if (isset($definition["arguments"])) {
+			$arguments = array_map(function ($arg) use ($context, $config) {
+				if (!is_string($arg)) { // what else could it be?
+					return $arg;
+				}
+
+				// %service
+				// %service%service%service%se...
+				if (substr($arg, 0, 1) === '%') {
+					$service = $context;
+					$arg = strtr($arg, '-', '\\'); // limited INI chars
+
+					do {
+						$service = $context->getService(substr($arg, 1, (stripos($arg, '%', 1) ?: strlen($arg))-1));
+						$arg = substr($arg, stripos($arg, '%', 1) ?: strlen($arg));
+					} while (substr($arg, 0, 1) === '%');
+
+					return $service;
+				}
+
+				// C$variable
+				if (substr($arg, 0, 2) === 'C$') {
+					return $config[substr($arg, 2)];
+				}
+
+				// E$variable
+				if (substr($arg, 0, 2) === 'E$') {
+					return \Nette\Environment::getVariable(substr($arg, 2));
+				}
+
+				return $arg;
+			}, $definition["arguments"]);
+		}
+
+		// class definition, without special initialization
+		if (isset($definition["class"])) {
+			if ($arguments) {
+				$ref = new \ReflectionClass($definition["class"]);
+			}
+
+			$object = $arguments ? $ref->newInstanceArgs($arguments) : new $definition["class"];
+		}
+
+		// objet factory
+		if (isset($definition["factory"])) {
+			$object = call_user_func_array($definition["factory"], $arguments);
+		}
+
+		// inject database manager
+		if ($object instanceof Kdyby\Doctrine\Service) {
+			$object->setDatabaseManager($context->getService('Kdyby\Application\DatabaseManager'));
+		}
+
+		return $object;
+	}
 
 
 
@@ -38,10 +161,11 @@ class Configurator extends Nette\Configurator
 	{
 		// setup session
 		if (!$session->isStarted()) {
-			$domain = Kdyby\Web\HttpHelpers::getDomain()->domain;
-			$session->setCookieParams('/', '.'.$domain);
+			if (!Environment::isConsole()){
+				$domain = Kdyby\Web\HttpHelpers::getDomain()->domain;
+				$session->setCookieParams('/', '.'.$domain);
+			}
 			$session->setExpiration(Nette\Tools::YEAR);
-
 			if (!$session->exists()) {
 				$session->start();
 			}
@@ -67,7 +191,7 @@ class Configurator extends Nette\Configurator
 	{
 		$hooks = self::$configHooks;
 
-		$kdybyConfig = Nette\Environment::getConfig('Kdyby');
+		$kdybyConfig = Environment::getConfig('Kdyby');
 		if (isset($kdybyConfig['Core'])) {
 			$hooks = $kdybyConfig['Core']->toArray() + $hooks;
 		}
@@ -82,12 +206,10 @@ class Configurator extends Nette\Configurator
 	 */
 	public static function createCacheStorage()
 	{
-		$context = new Nette\Context;
-		$context->addService('Nette\\Caching\\ICacheJournal', callback('Nette\Configurator::createCacheJournal'));
+		$dir = FileSystem::prepareWritableDir('%varDir%/cache');
 
-		$dir = FileSystem::prepareWritableDir('%tempDir%/cache');
-
-		return new Kdyby\FileStorage($dir, $context);
+		$journal = Environment::getService('Nette\\Caching\\ICacheJournal');
+		return new Kdyby\Caching\FileStorage($dir, $journal);
 	}
 
 
@@ -97,11 +219,10 @@ class Configurator extends Nette\Configurator
 	 */
 	public static function createMemcacheStorage($options)
 	{
-		$context = new Nette\Context;
-		$context->addService('Nette\\Caching\\ICacheJournal', callback('Kdyby\Configurator::createMemcacheJournal'));
+		$config = Environment::getConfig('memcache');
 
-		$config = Nette\Environment::getConfig('memcache');
-		return new \Nette\Caching\MemcachedStorage($config['host'], $config['port'], $options['prefix'], $context);
+		$journal = Environment::getService('Nette\Caching\IMemcacheJournal');
+		return new Nette\Caching\MemcachedStorage($config['host'], $config['port'], $options['prefix'], $journal);
 	}
 
 
@@ -122,7 +243,7 @@ class Configurator extends Nette\Configurator
 
 
 	/**
-	 * @return \Symfony\Component\HttpFoundation\UniversalClassLoader
+	 * @return Symfony\Component\HttpFoundation\UniversalClassLoader
 	 */
 	public static function createSymfony2Loader()
 	{
