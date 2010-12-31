ba78e558b66b18171f6bccd07b85f8501ecc3a51
Filip Procházka (HosipLan) <HosipLan@gmail.com>
2010-12-31 14:51:06 +0100

    Added idea for Repetitive Form Control

---
+++ Control/RepetitiveForm.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Kdyby\Control;
+
+use Nette;
+use Kdyby;
+
+
+
+abstract class RepetitiveForm extends LookoutControl
+{
+
+	/** @persistent */
+	public $fId;
+
+
+
+	/**
+	 * @param int|string $id
+	 * @return string
+	 */
+	protected function getSignalReceiverName($id)
+	{
+		return 'form'.$id;
+	}
+
+
+
+	/**
+	 * Pro renderování hromady formulářů pro každý competition zvlášť
+	 * @param array|object $defaults
+	 */
+	public function viewForm($defaults)
+	{
+		$id = is_array($defaults) ? $defaults['id'] : $defaults->id;
+		$form = $this->createAndAttach($id, $defaults);
+		$form->render();
+	}
+
+
+
+	/**
+	 * @param Nette\Application\Control $parent
+	 */
+	protected function attached($parent)
+	{
+		// vytvoříme formulář, aby mohl příjmout signál
+		// z principu stačí vytvořit ve výchozím stavu pouze jeden formulář,
+		// protože nette dovoluje zpracovat pouze jeden signál v jednom requestu
+		$this->createAndAttach($this->fId);
+		// o zbytek se postará životní cyklus
+
+		parent::attached($parent);
+	}
+
+
+
+	/**
+	 * @param int|string $id
+	 * @return Nette\Application\AppForm
+	 */
+	protected function createAndAttach($id, $defaults = NULL)
+	{
+		$name = $this->getSignalReceiverName($id);
+		$this[$name] = $this->createForm($id, $defaults);
+
+		// tady je trošku magie, možná to bude chtít poladit, aby to generovalo správný tvar
+		$fullname = $this->lookupPath(get_class($this));
+		$this[$name]->action->setParam($fullname . self::NAME_SEPARATOR . 'fId', $id);
+
+		return $this[$name];
+	}
+
+
+
+	abstract protected function createForm($id, $defaults = NULL);
+
+}
