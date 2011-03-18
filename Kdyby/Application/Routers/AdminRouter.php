4e2f08680744f508f2b2b6d35506966f44b1df08
Filip Procházka <hosiplan@gmail.com>
2011-03-18 16:57:39 +0100

    Added idea into AdminRouter

--- Kdyby/Application/Routers/AdminRouter.php
+++ Kdyby/Application/Routers/AdminRouter.php
@@ -17,8 +17,13 @@ class AdminRouter extends Nette\Application\Route
 	public function __construct($mask = NULL, $flags = 0)
 	{
 		parent::__construct(
+//				$mask . "/[<module>/][!<presenter>/][<action>/]",
 				$mask . "/[!<presenter>/][<action>/]",
 				$metadata = array(
+//					'module' => array(
+//						Route::FILTER_IN => callback($this, 'filterRequestsToAdminModule'),
+//						Route::FILTER_OUT => function ($module) { return $module; }, // prevent sanitization by urlencode
+//					),
 					'module' => 'Admin',
 					'presenter' => 'Dashboard',
 					'action' => 'default'
@@ -28,6 +33,22 @@ class AdminRouter extends Nette\Application\Route
 	}
 
 
+
+	/**
+	 * Checks if request directs to Admin Module
+	 * to prevent module in own key variable
+	 *
+	 * @param string $module
+	 * @return string|NULL
+	 */
+	public function filterRequestsToAdminModule($module)
+	{
+		if (!Nette\String::match($module, '~^\:?admin(\:.*)?$~i')) {
+			return NULL;
+		}
+
+		return $module;
+	}
 	
 
 }
