e67f915281e92dee630023d3b6304f39f9692e36
Filip Procházka <hosiplan@gmail.com>
2011-05-15 17:01:21 +0200

    Added Visual Paginator [Components]

---
+++ libs/Kdyby/Components/VisualPaginator/VisualPaginator.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Nette Framework Extras
+ *
+ * This source file is subject to the New BSD License.
+ *
+ * For more information please see http://addons.nette.org
+ *
+ * @copyright  Copyright (c) 2009 David Grudl
+ * @license    New BSD License
+ * @link       http://addons.nette.org
+ * @package    Nette Extras
+ */
+
+namespace Kdyby\Components\VisualPaginator;
+
+use Nette\Application\UI\Control;
+use Nette\Utils\Paginator;
+
+
+
+/**
+ * Visual paginator control.
+ *
+ * @author     David Grudl
+ * @copyright  Copyright (c) 2009 David Grudl
+ * @package    Nette Extras
+ */
+class VisualPaginator extends Control
+{
+
+	/** @var Paginator */
+	private $paginator;
+
+	/** @persistent */
+	public $page = 1;
+
+
+
+	/**
+	 * @return Nette\Paginator
+	 */
+	public function getPaginator()
+	{
+		if ($this->paginator === NULL) {
+			$this->paginator = new Paginator;
+		}
+
+		return $this->paginator;
+	}
+
+
+
+	/**
+	 * Renders paginator.
+	 * @return void
+	 */
+	public function render()
+	{
+		$paginator = $this->getPaginator();
+		$page = $paginator->page;
+		if ($paginator->pageCount < 2) {
+			$steps = array($page);
+
+		} else {
+			$arr = range(max($paginator->firstPage, $page - 3), min($paginator->lastPage, $page + 3));
+			$count = 4;
+			$quotient = ($paginator->pageCount - 1) / $count;
+			for ($i = 0; $i <= $count; $i++) {
+				$arr[] = round($quotient * $i) + $paginator->firstPage;
+			}
+			sort($arr);
+			$steps = array_values(array_unique($arr));
+		}
+
+		$this->template->steps = $steps;
+		$this->template->paginator = $paginator;
+		$this->template->setFile(dirname(__FILE__) . '/template.phtml');
+		$this->template->render();
+	}
+
+
+
+	/**
+	 * Loads state informations.
+	 * @param  array
+	 * @return void
+	 */
+	public function loadState(array $params)
+	{
+		parent::loadState($params);
+		$this->getPaginator()->page = $this->page;
+	}
+
+}
