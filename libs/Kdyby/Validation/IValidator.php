d3f924d4f4bbfc14afd6b6931a2b3548c13a0f59
Filip Procházka <hosiplan@gmail.com>
2011-05-15 16:58:14 +0200

    added IValidator [Validation]

---
+++ libs/Kdyby/Validation/IValidator.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008, 2011 Filip Procházka (filip.prochazka@kdyby.org)
+ *
+ * @license http://www.kdyby.org/license
+ */
+
+namespace Kdyby\Validation;
+
+
+
+/**
+ * @author Filip Procházka
+ */
+interface IValidator
+{
+
+	function validate($entity);
+
+}
