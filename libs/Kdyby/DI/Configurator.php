e725ba58d72c9f0432ca5e9f865851de9f85651f
Filip Procházka <hosiplan@gmail.com>
2011-05-20 18:15:03 +0200

    Updated configurator to include Doctrine\Container [DI] [Doctrine]

--- libs/Kdyby/DI/Configurator.php
+++ libs/Kdyby/DI/Configurator.php
@@ -13,4 +13,17 @@ use Nette;
 class Configurator extends Nette\Configurator
 {
 
+
+	/**
+	 * @param Nette\DI\Container $container
+	 * @return Kdyby\Doctrine\Container
+	 */
+	public static function createServiceDoctrine(Nette\DI\Container $container)
+	{
+		$doctrine = new Kdyby\Doctrine\Container;
+		$doctrine->container = $container;
+
+		return $doctrine;
+	}
+
 }
