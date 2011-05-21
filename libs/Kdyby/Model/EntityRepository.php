b1122175a052dbfc4c1591e895eba7926f69279d
Filip Procházka <hosiplan@gmail.com>
2011-05-21 20:01:45 +0200

    Added custom QueryBuilder to EntityRepository & Nette\Object behaviors to EntityRepository [Doctrine] [Model]

--- libs/Kdyby/Model/EntityRepository.php
+++ libs/Kdyby/Model/EntityRepository.php
@@ -13,6 +13,7 @@ namespace Kdyby\Model;
 use Doctrine;
 use Kdyby;
 use Nette;
+use Nette\ObjectMixin;
 
 
 
@@ -21,5 +22,67 @@ use Nette;
  */
 class EntityRepository extends Doctrine\ORM\EntityRepository
 {
+
+	/**
+	 * Create a new QueryBuilder instance that is prepopulated for this entity name
+	 *
+	 * @param string $alias
+	 * @return QueryBuilder $qb
+	 */
+	public function createQueryBuilder($alias)
+	{
+		$qb = new QueryBuilder($this->getEntityManager());
+		return $qb->select($alias)
+			->from($this->_entityName, $alias);
+	}
+
+
+
+	/********************* Nette\Object behaviour ****************d*g**/
+
+
+
+	/**
+	 * @return Nette\Reflection\ClassType
+	 */
+	public /**/static/**/ function getReflection()
+	{
+		return new Nette\Reflection\ClassType(/*5.2*$this*//**/get_called_class()/**/);
+	}
+
+
+
+	public function __call($name, $args)
+	{
+		return ObjectMixin::call($this, $name, $args);
+	}
+
+
+
+	public function &__get($name)
+	{
+		return ObjectMixin::get($this, $name);
+	}
+
+
+
+	public function __set($name, $value)
+	{
+		return ObjectMixin::set($this, $name, $value);
+	}
+
+
+
+	public function __isset($name)
+	{
+		return ObjectMixin::has($this, $name);
+	}
+
+
+
+	public function __unset($name)
+	{
+		ObjectMixin::remove($this, $name);
+	}
 
 }
---
+++ libs/Kdyby/Model/QueryBuilder.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Kdyby\Model;
+
+use Doctrine;
+use Kdyby;
+use Kdyby\Components\Grinder;
+use Nette;
+
+
+
+/**
+ * @author Filip Procházka
+ */
+class QueryBuilder extends Doctrine\ORM\QueryBuilder
+{
+
+	/**
+	 * Gets the root entity of the query. This is the first entity alias involved
+	 * in the construction of the query.
+	 *
+	 * <code>
+	 *     $qb = $em->createQueryBuilder()
+	 *         ->select('u')
+	 *         ->from('User', 'u');
+	 *
+	 *     echo $qb->getRootEntity(); // User
+	 * </code>
+	 *
+	 * @return string $rootEntity
+	 */
+	public function getRootEntity()
+	{
+		$from = $this->getDQLPart('from');
+		return $from[0]->getFrom();
+	}
+
+
+
+	/**
+	 * Get Gridito model
+	 * @return Grinder\Models\DoctrineQueryBuilderModel
+	 */
+	public function getGrinderModel()
+	{
+		$em = $this->getEntityManager();
+		$identifier = $em->getClassMetadata($this->getRootEntity())->getSingleIdentifierFieldName();
+
+		$grinderModel = new Grinder\DoctrineQueryBuilderModel($this);
+		$grinderModel->setPrimaryKey($identifier);
+
+		return $grinderModel;
+	}
+
+
+
+	/********************* Nette\Object behaviour ****************d*g**/
+
+
+
+	/**
+	 * @return Nette\Reflection\ClassType
+	 */
+	public /**/static/**/ function getReflection()
+	{
+		return new Nette\Reflection\ClassType(/*5.2*$this*//**/get_called_class()/**/);
+	}
+
+
+
+	public function __call($name, $args)
+	{
+		return ObjectMixin::call($this, $name, $args);
+	}
+
+
+
+	public function &__get($name)
+	{
+		return ObjectMixin::get($this, $name);
+	}
+
+
+
+	public function __set($name, $value)
+	{
+		return ObjectMixin::set($this, $name, $value);
+	}
+
+
+
+	public function __isset($name)
+	{
+		return ObjectMixin::has($this, $name);
+	}
+
+
+
+	public function __unset($name)
+	{
+		ObjectMixin::remove($this, $name);
+	}
+
+}
