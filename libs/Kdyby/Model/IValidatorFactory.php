4f4a844cada41a4171ef495bc61e3df5f9a9ab2a
Filip Procházka <hosiplan@gmail.com>
2011-06-15 07:09:24 +0200

    Added IValidatorFactory [Model]

---
+++ libs/Kdyby/Model/IValidatorFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008, 2011 Filip Procházka (filip.prochazka@kdyby.org)
+ *
+ * @license http://www.kdyby.org/license
+ */
+
+namespace Kdyby\Model;
+
+use Kdyby;
+use Nette;
+
+
+
+/**
+ * @author Filip Procházka
+ */
+interface IValidatorFactory
+{
+
+	/**
+	 * @return Kdyby\Validation\IValidator
+	 */
+	function createValidator();
+
+}
