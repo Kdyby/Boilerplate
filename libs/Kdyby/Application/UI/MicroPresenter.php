482ecc1025e134ce1c4f9dc4738b247efd01b14e
Filip Procházka <hosiplan@gmail.com>
2011-06-20 01:30:28 +0200

    Added mirror of NetteModule\MicroPresenter [UI]

---
+++ libs/Kdyby/Application/UI/MicroPresenter.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008, 2011 Filip Procházka (filip.prochazka@kdyby.org)
+ *
+ * @license http://www.kdyby.org/license
+ */
+
+namespace Kdyby\Application\UI;
+
+use Kdyby;
+use Nette;
+use Nette\Application;
+use Nette\Application\Responses;
+use Nette\Http;
+use NetteModule;
+
+
+
+/**
+ * @author Filip Procházka
+ */
+class MicroPresenter extends NetteModule\MicroPresenter
+{
+
+}
