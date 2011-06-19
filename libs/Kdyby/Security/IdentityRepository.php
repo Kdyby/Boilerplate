e0c21ad5b9557e7b582da69226aee02ff45d7c32
Filip Procházka <hosiplan@gmail.com>
2011-06-19 19:51:15 +0200

    Optimalized fetching of Identity [Security]

--- libs/Kdyby/Security/IdentityRepository.php
+++ libs/Kdyby/Security/IdentityRepository.php
@@ -12,6 +12,7 @@ namespace Kdyby\Security;
 
 use Doctrine\ORM\Query;
 use Doctrine\ORM\NoResultException;
+use Doctrine\DBAL\LockMode;
 use Kdyby;
 use Nette;
 
@@ -34,6 +35,43 @@ class IdentityRepository extends Kdyby\Model\EntityRepository implements IIdenti
 
 
 
+    /**
+     * Finds an entity by its primary key / identifier.
+     *
+     * @param int $id
+     * @return object
+     */
+    public function find($id, $lockMode = LockMode::NONE, $lockVersion = null)
+    {
+		if (func_num_args() > 1) {
+			throw new Nette\NotSupportedException("'" . __CLAS__ . "' doesn't support locking.");
+		}
+
+        // Check identity map first
+        if ($entity = $this->_em->getUnitOfWork()->tryGetById($id, $this->_class->rootEntityName)) {
+            if (!($entity instanceof $this->_class->name)) {
+                return NULL;
+            }
+
+            return $entity; // Hit!
+        }
+
+		$qb = $this->createQueryBuilder('u')
+			->addSelect('i')
+			->leftJoin('u.info', 'i')
+			->where('u.id = :id')
+			->setParameter('id', $id);
+
+		try {
+			return $qb->getQuery()->getSingleResult();
+
+		} catch (NoResultException $e) {
+			return NULL;
+		}
+    }
+
+
+
 	/**
 	 * @param string $nameOrEmail
 	 * @return Nette\Security\IIdentity
@@ -41,10 +79,11 @@ class IdentityRepository extends Kdyby\Model\EntityRepository implements IIdenti
 	public function findByNameOrEmail($nameOrEmail)
 	{
 		$qb = $this->createQueryBuilder('u')
-			->leftJoin('u.info')
+			->addSelect('i')
+			->leftJoin('u.info', 'i')
 			->where('u.username = :nameOrEmail')
-			->orWhere('u.email = :nameOrEmail');
-		$qb->setParameter('nameOrEmail', $nameOrEmail);
+			->orWhere('u.email = :nameOrEmail')
+			->setParameter('nameOrEmail', $nameOrEmail);
 
 		try {
 			return $qb->getQuery()->getSingleResult();
