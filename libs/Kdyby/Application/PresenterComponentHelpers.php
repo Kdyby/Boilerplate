14e11e776d14e7efc54d147cb84abaa2276be002
Filip Procházka <hosiplan@gmail.com>
2011-06-20 14:14:22 +0200

    Added PresenterComponentHelpers::nullLinkParams for traversing throught components and build array of nulled persistent params [Application] [Tools]

---
+++ libs/Kdyby/Application/PresenterComponentHelpers.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008, 2011 Filip Procházka (filip.prochazka@kdyby.org)
+ *
+ * @license http://www.kdyby.org/license
+ */
+
+namespace Kdyby\Application;
+
+use Kdyby;
+use Nette;
+
+
+
+/**
+ * @author Filip Procházka
+ */
+class PresenterComponentHelpers extends Nette\Object
+{
+
+	/**
+	 * Static class - cannot be instantiated.
+	 */
+	final public function __construct()
+	{
+		throw new Nette\StaticClassException("Cannot instantiate static class " . get_class($this));
+	}
+
+
+
+	/******************** Links ************************/
+
+
+
+	/**
+	 * @param Nette\Application\UI\PresenterComponent $component
+	 * @return array
+	 */
+	public static function nullLinkParams(Nette\Application\UI\PresenterComponent $component)
+	{
+		$parent = $component;
+		$presenter = $component instanceof Nette\Application\UI\Presenter ? NULL : $component->lookup('Nette\Application\UI\Presenter');
+		$params = array();
+
+		do {
+			if ($parent && method_exists($parent, 'getPersistentParams')) {
+				$name = $parent instanceof Nette\Application\UI\Presenter ? '' : $parent->lookupPath(get_class($presenter));
+
+				foreach ($parent->reflection->getPersistentParams() as $param => $info) {
+					$params[($name ? $name . $component::NAME_SEPARATOR : NULL) . $param] = $info['def'] ?: NULL;
+				}
+			}
+
+		} while($parent && $parent = $parent->getParent());
+
+		return $params;
+	}
+
+}
