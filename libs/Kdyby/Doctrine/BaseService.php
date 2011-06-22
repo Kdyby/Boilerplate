8b74fd2da8d8a006e08e8bfea5beb3ed82bd04d4
Filip Procházka <hosiplan@gmail.com>
2011-06-22 02:16:12 +0200

    Added base class for Services using Doctrine [Doctrine]

---
+++ libs/Kdyby/Doctrine/BaseService.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008, 2011 Filip Procházka (filip.prochazka@kdyby.org)
+ *
+ * @license http://www.kdyby.org/license
+ */
+
+namespace Kdyby\Doctrine;
+
+use Doctrine\ORM\EntityManager;
+use Kdyby;
+use Nette;
+
+
+
+/**
+ * @author Filip Procházka
+ *
+ * @property-read EntityManager $entityManager
+ */
+abstract class BaseService extends Nette\Object
+{
+
+	/** @var EntityManager */
+	private $entityManager;
+
+
+
+	/**
+	 * @param EntityManager $entityManager
+	 */
+	public function __construct(EntityManager $entityManager)
+	{
+		$this->entityManager = $entityManager;
+	}
+
+
+
+	/**
+	 * @return EntityManager
+	 */
+	public function getEntityManager()
+	{
+		return $this->entityManager;
+	}
+
+}
