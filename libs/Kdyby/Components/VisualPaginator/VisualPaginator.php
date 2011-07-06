577d68c00a4084811c2dec8de966ca13defac5d0
Filip Procházka <hosiplan@gmail.com>
2011-07-06 18:16:13 +0200

    VisualPaginator refactored [Components] [VisualPaginator]

--- libs/Kdyby/Components/VisualPaginator/VisualPaginator.php
+++ libs/Kdyby/Components/VisualPaginator/VisualPaginator.php
@@ -1,11 +1,7 @@
 <?php
 
 /**
- * Nette Framework Extras
- *
- * This source file is subject to the New BSD License.
- *
- * For more information please see http://addons.nette.org
+ * This file is part of the Kdyby (http://www.kdyby.org)
  *
  * @copyright  Copyright (c) 2009 David Grudl
  * @license    New BSD License
@@ -15,83 +11,19 @@
 
 namespace Kdyby\Components\VisualPaginator;
 
-use Nette\Application\UI\Control;
-use Nette\Utils\Paginator;
-
 
 
 /**
  * Visual paginator control.
  *
- * @author     David Grudl
- * @copyright  Copyright (c) 2009 David Grudl
- * @package    Nette Extras
+ * @author David Grudl
+ * @author Filip Procházka
  */
-class VisualPaginator extends Control
+class VisualPaginator extends ComponentPaginator
 {
-
-	/** @var Paginator */
-	private $paginator;
 
 	/** @persistent */
 	public $page = 1;
-
-
-
-	/**
-	 * @return Nette\Paginator
-	 */
-	public function getPaginator()
-	{
-		if ($this->paginator === NULL) {
-			$this->paginator = new Paginator;
-		}
-
-		return $this->paginator;
-	}
-
-
-
-	/**
-	 * Renders paginator.
-	 * @return void
-	 */
-	public function render()
-	{
-		echo $this->__toString();
-	}
-
-
-
-	/**
-	 * @return string
-	 */
-	public function __toString()
-	{
-		$paginator = $this->getPaginator();
-		$page = $paginator->page;
-		if ($paginator->pageCount < 2) {
-			$steps = array($page);
-
-		} else {
-			$arr = range(max($paginator->firstPage, $page - 3), min($paginator->lastPage, $page + 3));
-			$count = 4;
-			$quotient = ($paginator->pageCount - 1) / $count;
-			for ($i = 0; $i <= $count; $i++) {
-				$arr[] = round($quotient * $i) + $paginator->firstPage;
-			}
-			sort($arr);
-			$steps = array_values(array_unique($arr));
-		}
-
-		$this->template->steps = $steps;
-		$this->template->paginator = $paginator;
-		
-		if($this->template->getFile() === NULL){
-			$this->template->setFile(dirname(__FILE__) . '/template.phtml');
-		}
-		return (string)$this->template;
-	}
 
 
 
@@ -103,16 +35,7 @@ class VisualPaginator extends Control
 	public function loadState(array $params)
 	{
 		parent::loadState($params);
-		$this->getPaginator()->page = $this->page;
-	}
-	
-	/**
-	 * @param string
-	 * @return VisualPaginator
-	 */
-	public function setTemplateFile($file){
-		$this->getTemplate()->setFile($file);
-		return $this;
+		$this->setPage($this->page);
 	}
 
 }
