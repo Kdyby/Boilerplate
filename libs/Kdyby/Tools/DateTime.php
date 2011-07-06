3f819a661c63f0de4976f579b15a4eeecbd71784
Filip Procházka <hosiplan@gmail.com>
2011-07-06 18:17:00 +0200

    Added printable DateTime [Tools]

---
+++ libs/Kdyby/Tools/DateTime.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008, 2011 Filip Procházka (filip.prochazka@kdyby.org)
+ *
+ * @license http://www.kdyby.org/license
+ */
+
+namespace Kdyby\Tools;
+
+use Kdyby;
+use Nette;
+
+
+
+/**
+ * @author Filip Procházka
+ *
+ * @param string $defaultFormat
+ */
+class DateTime extends Nette\DateTime
+{
+
+	/** @var string */
+	private $defaultFormat = 'j.n.Y G:i';
+
+
+
+	/**
+	 * DateTime object factory.
+	 * @param  string|int|\DateTime
+	 * @return DateTime
+	 */
+	public static function from($time)
+	{
+		if ($time instanceof \DateTime) {
+			return new static(
+					date('Y-m-d H:i:s', $time->getTimestamp()),
+					$time->getTimezone()
+				);
+		}
+
+		return parent::from($time);
+	}
+
+
+
+	/**
+	 * @param string $defaultFormat
+	 * @return DateTime
+	 */
+	public function setDefaultFormat($defaultFormat)
+	{
+		$this->defaultFormat = (string)$defaultFormat;
+		return $this;
+	}
+
+
+
+	/**
+	 * @return string
+	 */
+	public function getDefaultFormat()
+	{
+		return $this->defaultFormat;
+	}
+
+
+
+	/**
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return $this->format($this->defaultFormat);
+	}
+
+}
