eb88efc871eecea42db62ea04f424cec762404c0
Filip Procházka <hosiplan@gmail.com>
2011-07-12 16:36:16 +0200

    Testing Assertion for Nette\Object's events [Tests]

--- libs/Kdyby/Testing/Test.php
+++ libs/Kdyby/Testing/Test.php
@@ -21,4 +21,49 @@ use Kdyby;
 class Test extends \PHPUnit_Framework_TestCase
 {
 
+	/**
+	 * @param array|\Nette\Callback|\Closure $callback
+	 * @param Nette\Object $object
+	 * @param string $eventName
+	 */
+	public function assertEventHasCallback($callback, $object, $eventName)
+	{
+		$this->assertInstanceOf('Nette\Object', $object, 'Object supports events');
+		$this->assertObjectHasAttribute($eventName, $object, 'Object has event');
+
+		$event = $object->$eventName;
+		$this->assertNotEmpty($event, 'Event contains listeners');
+
+		if (is_array($callback)) {
+			$this->assertContainsOnly('array', $event, TRUE, 'Event contains only arrays');
+
+		} elseif ($callback instanceof Nette\Callback) {
+			$this->assertContainsOnly('Nette\Callback', $event, FALSE, 'Event contains only instances of Nette\Callback');
+		}
+
+		$targetIndex = array_search($callback, $event);
+		$this->assertNotNull($targetIndex, 'Similar listener is in event');
+		$target = $event[$targetIndex];
+
+		if (is_array($callback)) {
+			$this->assertNotNull($target[0]);
+			$this->assertSame($callback[0], $target[0], 'Target matches');
+
+			if (isset($callback[1])) {
+				$this->assertNotNull($target[1]);
+				$this->assertSame($callback[1], $target[1], 'Target matches');
+			}
+		}
+
+		if ($callback instanceof Nette\Callback) {
+			$this->assertNotNull($target->native[0]);
+			$this->assertSame($callback->native[0], $target->native[0], 'Target matches');
+
+			if (isset($callback->native[1])) {
+				$this->assertNotNull($target->native[1]);
+				$this->assertSame($callback->native[1], $target->native[1], 'Target matches');
+			}
+		}
+	}
+
 }
