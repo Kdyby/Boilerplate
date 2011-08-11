91b2ab5dd884ac5dffd7f54496f2574dd49423cf
Filip Procházka <hosiplan@gmail.com>
2011-08-11 13:25:52 +0200

    SelectIterator is similar to FilterIterator, but with multiple callbacks [Iterators]

---
+++ libs/Kdyby/Iterators/SelectIterator.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008, 2011 Filip Procházka (filip.prochazka@kdyby.org)
+ *
+ * @license http://www.kdyby.org/license
+ */
+
+namespace Kdyby\Iterators;
+
+use Kdyby;
+use Nette;
+
+
+
+/**
+ * @author Filip Procházka
+ */
+class SelectIterator extends \FilterIterator
+{
+
+	/** @var callback[] */
+	private $filters;
+
+
+
+	/**
+	 * @param callable $callback
+	 * @return CollectIterator
+	 */
+	public function select($callback)
+	{
+		$this->filters[] = callback($callback);
+		return $this;
+	}
+
+
+
+	/**
+	 * @return boolean
+	 */
+	public function accept()
+	{
+		foreach ($this->filters as $filter) {
+			if (!$filter($this)) {
+				return FALSE;
+			}
+		}
+
+		return TRUE;
+	}
+
+
+
+	/**
+	 * @return array
+	 */
+	public function toArray()
+	{
+		return iterator_to_array($this);
+	}
+
+}
