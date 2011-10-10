dc61d17c7577de86a87b1b99c01d1a923bd4d554
Filip Procházka <hosiplan@gmail.com>
2011-10-10 16:18:36 +0200

    Identity Query [Security]

---
+++ libs/Kdyby/Security/IdentityByNameOrEmailQuery.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008, 2011 Filip Procházka (filip.prochazka@kdyby.org)
+ *
+ * @license http://www.kdyby.org/license
+ */
+
+namespace Kdyby\Security;
+
+use Doctrine;
+use Kdyby\Doctrine\IQueryable;
+use Kdyby;
+use Nette;
+use Nette\Utils\Paginator;
+
+
+
+/**
+ * @author Filip Procházka
+ */
+class IdentityByNameOrEmailQuery extends Kdyby\Doctrine\ORM\QueryObjectBase
+{
+
+	/** @var string */
+	private $nameOrEmail;
+
+
+
+	/**
+	 * @param string $nameOrEmail
+	 * @param Paginator $paginator
+	 */
+	public function __construct($nameOrEmail, Paginator $paginator = NULL)
+	{
+		parent::__construct($paginator);
+		$this->nameOrEmail = $nameOrEmail;
+	}
+
+
+
+	/**
+	 * @return string
+	 */
+	public function getNameOrEmail()
+	{
+		return $this->nameOrEmail;
+	}
+
+
+
+	/**
+	 * @param IQueryable $repository
+	 * @return Doctrine\ORM\QueryBuilder
+	 */
+	protected function doCreateQuery(IQueryable $repository)
+	{
+		return $this->createQueryBuilder('u')
+			->where('u.username = :nameOrEmail')
+			->orWhere('u.email = :nameOrEmail')
+			->setParameter('nameOrEmail', $this->nameOrEmail);
+	}
+
+}
