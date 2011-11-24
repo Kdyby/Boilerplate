6ccc0f400008bd8ccceb75c72fbe5cf4b8caacd7
Filip Procházka <hosiplan@gmail.com>
2011-11-24 10:48:37 +0100

    Upgraded Doctrine version to 2.1.4 [vendors]

--- vendors.php
+++ vendors.php
@@ -18,7 +18,7 @@ if (!is_dir($vendorDir = __DIR__ . '/vendor')) {
 $deps = array(
 	array('nette', 'http://github.com/nette/nette.git', 'origin/HEAD'),
 	array('symfony', 'http://github.com/symfony/symfony.git', 'v2.0.6'),
-	array('doctrine', 'http://github.com/doctrine/doctrine2.git', '2.1.2'),
+	array('doctrine', 'http://github.com/doctrine/doctrine2.git', '2.1.4'),
 	array('doctrine-dbal', 'http://github.com/doctrine/dbal.git', '2.1.3'),
 	array('doctrine-common', 'http://github.com/doctrine/common.git', '2.1.2'),
 	array('doctrine-data-fixtures', 'http://github.com/doctrine/data-fixtures.git', 'origin/HEAD'),
@@ -34,10 +34,10 @@ foreach ($deps as $dep) {
 
 	$installDir = $vendorDir . '/' . $name;
 	if (!is_dir($installDir)) {
-		echo "> Installing $name\n";
+		echo "$ install $name\n";
 		system(sprintf('git clone %s %s', escapeshellarg($url), escapeshellarg($installDir)));
 	}
 
-	echo "> Updating $name\n";
+	echo "$ update $name\n";
 	system(sprintf('cd %s && git fetch origin && git reset --hard %s', escapeshellarg($installDir), escapeshellarg($rev)));
 }
