1ca06e7e97fb3ed7ae57fbf5b2051d67c30c4320
Filip Procházka <hosiplan@gmail.com>
2011-12-28 16:02:05 +0100

    fd() function for dumping into file [Diagnostics]

--- libs/Kdyby/functions.php
+++ libs/Kdyby/functions.php
@@ -15,6 +15,49 @@ use Nette\Diagnostics\Helpers;
 
 
 /**
+ * Outputs the variable content to file
+ *
+ * @author Filip Procházka <filip.prochazka@kdyby.org>
+ *
+ * @param mixed $variable
+ * @return mixed
+ */
+function fd($variable) {
+	static $style = <<<CSS
+	pre.nette-dump {
+		color: #444; background: white;
+	}
+	pre.nette-dump .php-array, pre.nette-dump .php-object {
+		color: #C22;
+	}
+	pre.nette-dump .php-string {
+		color: #080;
+	}
+	pre.nette-dump .php-int, pre.nette-dump .php-float {
+		color: #37D;
+	}
+	pre.nette-dump .php-null, pre.nette-dump .php-bool {
+		color: black;
+	}
+	pre.nette-dump .php-visibility {
+		font-size: 85%; color: #999;
+	}
+CSS;
+
+	$dump = "<pre class=\"nette-dump\">" . Nette\Diagnostics\Helpers::htmlDump($variable) . "</pre>\n";
+	$dump .= "<style>" . $style . "</style>";
+
+	$file = Debugger::$logDirectory . '/dump_' . substr(md5($dump), 0, 6) . '.html';
+
+	file_put_contents($file, $dump);
+	Kdyby\Diagnostics\ConsoleDebugger::openFile($file);
+
+	return $variable;
+}
+
+
+
+/**
  * Bar dump shortcut.
  * @see Nette\Diagnostics\Debugger::barDump
  * @author Filip Procházka <filip.prochazka@kdyby.org>
