74c22d1fd0677a07aebd70a98d51ff4c65bbccbd
Filip Procházka <hosiplan@gmail.com>
2011-12-29 00:59:17 +0100

    Function fd() now can dump more deeply

--- libs/Kdyby/functions.php
+++ libs/Kdyby/functions.php
@@ -22,31 +22,21 @@ use Nette\Diagnostics\Helpers;
  * @param mixed $variable
  * @return mixed
  */
-function fd($variable) {
+function fd($variable, $maxDepth = 3) {
 	static $style = <<<CSS
-	pre.nette-dump {
-		color: #444; background: white;
-	}
-	pre.nette-dump .php-array, pre.nette-dump .php-object {
-		color: #C22;
-	}
-	pre.nette-dump .php-string {
-		color: #080;
-	}
-	pre.nette-dump .php-int, pre.nette-dump .php-float {
-		color: #37D;
-	}
-	pre.nette-dump .php-null, pre.nette-dump .php-bool {
-		color: black;
-	}
-	pre.nette-dump .php-visibility {
-		font-size: 85%; color: #999;
-	}
+	pre.nette-dump { color: #444; background: white; }
+	pre.nette-dump .php-array, pre.nette-dump .php-object { color: #C22; }
+	pre.nette-dump .php-string { color: #080; }
+	pre.nette-dump .php-int, pre.nette-dump .php-float { color: #37D; }
+	pre.nette-dump .php-null, pre.nette-dump .php-bool { color: black; }
+	pre.nette-dump .php-visibility { font-size: 85%; color: #999; }
 CSS;
 
+	$originalDepth = Debugger::$maxDepth;
+	Debugger::$maxDepth = $maxDepth;
 	$dump = "<pre class=\"nette-dump\">" . Nette\Diagnostics\Helpers::htmlDump($variable) . "</pre>\n";
+	Debugger::$maxDepth = $originalDepth;
 	$dump .= "<style>" . $style . "</style>";
-
 	$file = Debugger::$logDirectory . '/dump_' . substr(md5($dump), 0, 6) . '.html';
 
 	file_put_contents($file, $dump);
