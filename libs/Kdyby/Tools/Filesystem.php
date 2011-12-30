d04a79e7b016e85b7adb806b2f67d833518c2e13
Filip Procházka <hosiplan@gmail.com>
2011-12-30 22:48:46 +0100

    Added writing to Filesystem [Tools]

--- libs/Kdyby/Tools/Filesystem.php
+++ libs/Kdyby/Tools/Filesystem.php
@@ -35,45 +35,105 @@ final class Filesystem extends Nette\Object
 
 	/**
 	 * @param string $file
+	 * @param bool $need
+	 *
+	 * @return bool
+	 * @throws \Kdyby\FileNotWritableException
 	 */
-	public static function rm($file)
+	public static function rm($file, $need = TRUE)
 	{
-		@unlink((string)$file);
+		if (is_dir((string)$file)) {
+			return static::rmDir($file, FALSE, $need);
+		}
+
+		if (FALSE === ($result = @unlink((string)$file)) && $need) {
+			throw new Kdyby\FileNotWritableException("Unable to delete file '$file'");
+		}
+
+		return $result;
 	}
 
 
 
 	/**
-	 * @param string $directory
+	 * @param string $dir
 	 * @param bool $recursive
+	 * @param bool $need
 	 *
 	 * @return boolean
+	 * @throws \Kdyby\DirectoryNotWritableException
 	 */
-	public static function rmDir($directory, $recursive = TRUE)
+	public static function rmDir($dir, $recursive = TRUE, $need = TRUE)
 	{
-		$recursive && self::cleanDir((string)$directory);
-		return @rmdir((string)$directory);
+		$recursive && self::cleanDir($dir = (string)$dir, $need);
+		if (is_dir($dir) && FALSE === ($result = @rmdir($dir)) && $need) {
+			throw new Kdyby\DirectoryNotWritableException("Unable to delete directory '$dir'.");
+		}
+
+		return isset($result) ? $result : TRUE;
 	}
 
 
 
 	/**
-	 * @param string $directory
+	 * @param string $dir
+	 * @param bool $need
+	 *
+	 * @return bool
 	 */
-	public static function cleanDir($directory)
+	public static function cleanDir($dir, $need = TRUE)
 	{
-		if (!file_exists($directory)) {
-			return;
+		if (!file_exists($dir)) {
+			return TRUE;
 		}
 
-		foreach (Nette\Utils\Finder::find('*')->from($directory)->childFirst() as $file) {
-			if ($file->isDir()) {
-				static::rmDir($file, FALSE);
-
-			} else {
-				static::rm($file);
+		foreach (Nette\Utils\Finder::find('*')->from($dir)->childFirst() as $file) {
+			if (FALSE === static::rm($file, $need)) {
+				return FALSE;
 			}
 		}
+
+		return TRUE;
+	}
+
+
+
+	/**
+	 * @param string $dir
+	 * @param bool $recursive
+	 * @param int $chmod
+	 * @param bool $need
+	 *
+	 * @throws \Kdyby\IOException
+	 */
+	public static function mkDir($dir, $recursive = TRUE, $chmod = 0777, $need = TRUE)
+	{
+		if (!is_dir($dir) && FALSE === ($result = @mkdir($dir, $chmod, $recursive)) && $need) {
+			throw new Kdyby\IOException('Unable to create directory ' . $dir);
+		}
+
+		return isset($result) ? $result : TRUE;
+	}
+
+
+
+	/**
+	 * @param string $file
+	 * @param string $contents
+	 * @param bool $createDirectory
+	 * @param bool $need
+	 *
+	 * @throws \Kdyby\FileNotWritableException
+	 */
+	public static function write($file, $contents, $createDirectory = TRUE, $need = TRUE)
+	{
+		$createDirectory && static::mkDir(dirname($file));
+
+		if (FALSE === ($result = @file_put_contents($file, $contents)) && $need) {
+			throw Kdyby\FileNotWritableException::fromFile($file);
+		}
+
+		return $result;
 	}
 
 }
