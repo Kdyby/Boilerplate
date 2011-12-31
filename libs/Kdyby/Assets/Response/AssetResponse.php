e4e7b5743d0a82d237cc1b8ba674c7a67d3431df
Filip Procházka <hosiplan@gmail.com>
2011-12-31 19:53:35 +0100

    Fixed license

--- libs/Kdyby/Assets/Response/AssetResponse.php
+++ libs/Kdyby/Assets/Response/AssetResponse.php
@@ -1,12 +1,11 @@
 <?php
 
 /**
- * This file is part of the Nette Framework (http://nette.org)
+ * This file is part of the Kdyby (http://www.kdyby.org)
  *
- * Copyright (c) 2004, 2011 David Grudl (http://davidgrudl.com)
+ * Copyright (c) 2008, 2011 Filip Procházka (filip.prochazka@kdyby.org)
  *
- * For the full copyright and license information, please view
- * the file license.txt that was distributed with this source code.
+ * @license http://www.kdyby.org/license
  */
 
 namespace Kdyby\Assets\Response;
