f4d6d6032e700277f91485fc638fa56cc16af852
Filip Procházka <hosiplan@gmail.com>
2012-01-18 16:21:47 +0100

    Added missing class [Security]

---
+++ libs/Kdyby/Security/Permission.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008, 2011 Filip Procházka (filip.prochazka@kdyby.org)
+ *
+ * @license http://www.kdyby.org/license
+ */
+
+namespace Kdyby\Security;
+
+use Kdyby;
+use Nette;
+
+
+
+/**
+ * @todo: create Security\IOwner; automatic attachment of assertion to check owner using IOwner
+ *
+ * @author Filip Procházka <filip.prochazka@kdyby.org>
+ */
+class Permission extends Nette\Security\Permission
+{
+
+}
