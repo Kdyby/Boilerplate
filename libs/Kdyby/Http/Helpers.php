5ce564eb1ec8d77c6fb1aa402b2b9936ae597aa2
Filip Procházka <hosiplan@gmail.com>
2012-03-30 13:58:39 +0200

    Helpers for redirecting to www. or non-www variant of domain without rewrites [Http]

--- libs/Kdyby/Http/Helpers.php
+++ libs/Kdyby/Http/Helpers.php
@@ -24,17 +24,49 @@ class Helpers extends Nette\Object
 {
 
 	/** @var string */
-	const DOMAIN_PATTERN = '~^(?:(?P<second>[^.]+)+\.)?(?P<domain>(?P<top>[^.]+)\.(?P<tld>[^.]+))$~i';
+	const DOMAIN_PATTERN = '~^(?P<subdomain>.*?\.)?(?P<domain>[^.]+\.[^.]+)$~';
 
 
 
 	/**
-	 * @param Http\Request $httpRequest
-	 * @return object
+	 * @param \Nette\Http\Request $httpRequest
+	 * @return array
 	 */
 	public static function getDomain(Http\Request $httpRequest)
 	{
-		return (object)Strings::match($httpRequest->url->host, self::DOMAIN_PATTERN);
+		return Strings::match($httpRequest->url->host, self::DOMAIN_PATTERN);
+	}
+
+
+
+	/**
+	 * @param \Nette\Http\Request $httpRequest
+	 * @param \Nette\Http\Response $httpResponse
+	 */
+	public static function wwwRedirect(Http\Request $httpRequest, Http\Response $httpResponse)
+	{
+		$url = $httpRequest->url;
+		if (substr($url->host, 0, 4) !== 'www.' && $host = static::getDomain($httpRequest)) {
+			$url = clone $url;
+			$url->host = 'www.' . $host['domain'];
+			$httpResponse->redirect((string)$url);
+		}
+	}
+
+
+
+	/**
+	 * @param \Nette\Http\Request $httpRequest
+	 * @param \Nette\Http\Response $httpResponse
+	 */
+	public static function nonWwwRedirect(Http\Request $httpRequest, Http\Response $httpResponse)
+	{
+		$url = $httpRequest->url;
+		if (substr($url->host, 0, 4) === 'www.' && $host = static::getDomain($httpRequest)) {
+			$url = clone $url;
+			$url->host = substr($host['domain'], 4, 0);
+			$httpResponse->redirect((string)$url);
+		}
 	}
 
 }
