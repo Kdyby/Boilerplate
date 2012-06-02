7a1036fbb07810a962b084c221a517b6a8485612
Filip Procházka <hosiplan@gmail.com>
2012-06-02 17:50:39 +0200

    fixed nette.ajax.codeEditor.js handler [FrameworkPackage]

--- libs/Kdyby/Package/FrameworkPackage/Resources/public/js/nette.ajax.codeEditor.js
+++ libs/Kdyby/Package/FrameworkPackage/Resources/public/js/nette.ajax.codeEditor.js
@@ -15,13 +15,15 @@
 	/**
 	 */
 	$.nette.ext('codeEditor', {
-		before: function (ui) {
+		before: function (settings, ui) {
 			if (ui) {
-				var $form = $(ui).closest('form').find('textarea.code-editor').each(function () {
+				var $editors = $(ui).closest('form').find('textarea.code-editor');
+				$editors.each(function () {
 					var $editor = $(this);
 					var codeEditor = $editor.data('code-editor');
 					if (codeEditor) {
 						$editor.val(codeEditor.getValue());
+						settings.data[$editor.attr('name')] = $editor.val();
 					}
 				});
 			}
