f846d22b7c188c99d01493fa51830e9cec81f157
Filip Procházka <hosiplan@gmail.com>
2012-08-27 17:34:44 +0200

    doc

--- src/Facebook/Dialog.php
+++ src/Facebook/Dialog.php
@@ -7,6 +7,11 @@ use Nette;
 
 
 /**
+ * Dialogs provide a simple, consistent interface to provide social functionality to your users.
+ * Dialogs do not require any additional permissions because they require user interaction.
+ * Dialogs can be used by your application in every context: within a Canvas Page, in a Page Tab,
+ * in a website or mobile web app, and within native iOS and native Android applications.
+ *
  * @author Filip Procházka <filip.prochazka@kdyby.org>
  */
 interface Dialog
