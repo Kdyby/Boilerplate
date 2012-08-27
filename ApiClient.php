05b42d261caff72e606d48686d63dec655475c5c
Filip Procházka <hosiplan@gmail.com>
2012-08-28 01:39:46 +0200

    licence header

--- ApiClient.php
+++ ApiClient.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008, 2011 Filip Procházka (filip.prochazka@kdyby.org)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
 namespace Kdyby\Extension\Social\Facebook;
 
 
