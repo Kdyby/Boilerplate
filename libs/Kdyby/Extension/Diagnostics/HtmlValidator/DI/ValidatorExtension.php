214bd5abc9d0ac69bbdce938961b77481c79e2e9
Filip Procházka <hosiplan@gmail.com>
2012-09-04 19:33:13 +0200

    Panel for validating the output HTML [HtmlValidator]

---
+++ libs/Kdyby/Extension/Diagnostics/HtmlValidator/DI/ValidatorExtension.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008, 2011 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\Extension\Diagnostics\HtmlValidator\DI;
+
+use Kdyby;
+use Nette;
+use Nette\Config\Compiler;
+use Nette\Config\Configurator;
+
+
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+class ValidatorExtension extends Nette\Config\CompilerExtension
+{
+
+	public function loadConfiguration()
+	{
+		$builder = $this->getContainerBuilder();
+		if (!$builder->parameters['debugMode']) {
+			return;
+		}
+
+		$builder->addDefinition($this->prefix('panel'))
+			->setClass('Kdyby\Extension\Diagnostics\HtmlValidator')
+			->addSetup('Nette\Diagnostics\Debugger::$bar->addPanel(?)', array('@self'));
+
+		$builder->getDefinition('application')
+			->addSetup('$service->onStartup[] = ?', array(array('@validatorPanel', 'startBuffering')))
+			->addSetup('$service->onShutdown[] = ?', array(array('@validatorPanel', 'validate')))
+			->addSetup('$service->onError[] = ?', array(array('@validatorPanel', 'stopBuffering')));
+	}
+
+
+
+	/**
+	 * @param \Nette\Config\Configurator $config
+	 */
+	public static function register(Configurator $config)
+	{
+		$config->onCompile[] = function (Configurator $config, Compiler $compiler) {
+			$compiler->addExtension('htmlValidator', new ValidatorExtension());
+		};
+	}
+
+}
