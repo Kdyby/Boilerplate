4244b3fe28d314b16237f2a3691e41c272c6f41b
Filip Procházka <hosiplan@gmail.com>
2012-09-22 13:31:44 +0200

    refactored autoload of tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,27 @@
 <?php
 
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008, 2012 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.md that was distributed with this source code.
+ */
+
 // take care of autoloading
-require_once __DIR__ . '/../autoload.php';
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+// require class loader
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader = require_once __DIR__ . '/../vendor/autoload.php';
+$loader->add('Kdyby\\Tests', __DIR__);
+$loader->add('Kdyby', __DIR__ . '/../libs');
+
+// Doctrine annotations
+AnnotationRegistry::registerLoader(callback('class_exists'));
 
 // create container
 Kdyby\Tests\Configurator::testsInit(__DIR__)
 	->getContainer();
+
+unset($loader); // cleanup
